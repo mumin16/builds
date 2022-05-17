99365cfa9e0e0fb5792b1553c7c476d584826aae
mumin guler <mumin16@hotmail.com>
2022-05-17 11:37:11 +0300

    Add files via upload

---
+++ build_openssl.sh
@@ -0,0 +1,8 @@
+export ANDROID_NDK_ROOT=/home/x64/Android/Sdk/ndk/24.0.8215888
+export PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
+export CC=clang
+
+#android-arm64 or android-x86_64
+perl Configure android-arm64 -D_ANDROID_API=android-26 no-shared
+
+make
