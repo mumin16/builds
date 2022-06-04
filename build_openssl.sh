2c538c92d49ee6e896a14f745a6ab7f8392a341d
mumin guler <mumin16@hotmail.com>
2022-06-04 22:37:57 +0300

    Update build_openssl.sh

--- build_openssl.sh
+++ build_openssl.sh
@@ -4,5 +4,6 @@ export CC=clang
 
 #android-arm64 or android-x86_64
 perl Configure android-arm64 -D_ANDROID_API=android-26 no-shared
-
+#perl Configure ios64-xcrun no-shared 
+#perl Configure iossimulator-xcrun no-shared 
 make
