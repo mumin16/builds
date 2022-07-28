a73bdeec0b3d37f029a3de9132109dd0b3c54715
mumin guler <mumin16@hotmail.com>
2022-07-28 21:36:56 +0300

    Update build_openssl.sh

--- build_openssl.sh
+++ build_openssl.sh
@@ -1,8 +1,8 @@
-export ANDROID_NDK_ROOT=/home/x64/Android/Sdk/ndk/24.0.8215888
+export ANDROID_NDK_ROOT=/home/x64/Android/Sdk/ndk/25.0.8775105
 export PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
 export CC=clang
 
-#android-arm64 or android-x86_64
+#android-arm64 or android-x86_64 or android-x86
 perl Configure android-arm64 -D_ANDROID_API=android-26 no-shared
 #perl Configure ios64-xcrun no-shared 
 #perl Configure iossimulator-xcrun no-shared 
